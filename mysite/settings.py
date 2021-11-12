4bd322fb8733275e866e044e34c4a871be2c5350
Ömer Faruk <omerfarukavci@yandex.com>
2021-11-12 17:25:17 +0300

    base.html file edited

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i$=bl)f0o#%6fz71kr4p)4_^jp0^+3=ixrhe82d#+-h)&whd#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
